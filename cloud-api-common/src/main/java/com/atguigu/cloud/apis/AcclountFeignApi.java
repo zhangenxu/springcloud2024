28c8d6f8c25b2a14b60702489d63ee954e120e74
enxu.zhang <enxu.zhang@yeepay.com>
2024-05-28 13:46:26 +0800

    Changes

--- cloud-api-common/src/main/java/com/atguigu/cloud/apis/AcclountFeignApi.java
+++ cloud-api-common/src/main/java/com/atguigu/cloud/apis/AcclountFeignApi.java
@@ -2,9 +2,7 @@ package com.atguigu.cloud.apis;
 
 import com.atguigu.cloud.resp.Result;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.math.BigDecimal;
